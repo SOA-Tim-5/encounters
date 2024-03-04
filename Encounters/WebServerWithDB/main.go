259cf7bc14ec99d1fe112d4d88fae3f531f6f02c
Milica Vujić <milicavujic2001@gmail.com>
2024-03-04 18:33:34 +0100

    chore: DB connection string updated.

--- Encounters/WebServerWithDB/main.go
+++ Encounters/WebServerWithDB/main.go
@@ -7,24 +7,29 @@ import (
 	"database-example/service"
 	"log"
 	"net/http"
+	"gorm.io/driver/postgres"
 
 	"github.com/gorilla/mux"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func initDB() *gorm.DB {
-	//connectionStr := "root:root@tcp(localhost:3306)/students?charset=utf8mb4&parseTime=True&loc=Local"
-	connectionStr := "postgres://postgres:super@localhost:5432/explorer-v1?sslmode=disable"
+	dsn := "user=postgres password=super dbname=explorer-v1 host=localhost port=5432 sslmode=disable search_path=encounters"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	database, err := gorm.Open(mysql.Open(connectionStr), &gorm.Config{})
 	if err != nil {
 		print(err)
 		return nil
 	}
 
 	database.AutoMigrate(&model.Student{})
-	database.Exec("INSERT IGNORE INTO students VALUES ('aec7e123-233d-4a09-a289-75308ea5b7e6', 'Marko Markovic', 'Graficki dizajn')")
+
+	err = database.AutoMigrate(&model.Student{})
+	if err != nil {
+		log.Fatalf("Error migrating models: %v", err)
+	}
+
+
 	return database
 }
 
